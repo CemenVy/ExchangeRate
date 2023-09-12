ee77fd4da8139c44079ab73a91fee1326768ff99
CemenVy <semen.vydrin0108@gmail.com>
2023-09-12 14:35:51 +0400

    Project settings

---
+++ ExchangeRate/Models/Rate.swift
@@ -0,0 +1,8 @@
+//
+//  Rate.swift
+//  ExchangeRate
+//
+//  Created by Семен Выдрин on 12.09.2023.
+//
+
+import Foundation
---
+++ ExchangeRate/Services/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  ExchangeRate
+//
+//  Created by Семен Выдрин on 12.09.2023.
+//
+
+import Foundation
